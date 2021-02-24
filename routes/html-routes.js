4c1a7d27773b81b2131b87b6c57d5ab94e421f2f
Des <d.stout31@yahoo.com>
2021-02-23 20:32:49 -0600

    user account and login is working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,27 +1,29 @@
-// Dependencies
-const path = require('path');
+// Requiring path to so we can use relative routes to our HTML files
+const path = require("path");
 
-// Routes
-module.exports = (app) => {
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+// Requiring our custom middleware for checking if a user is logged in
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-  // index route loads view.html
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/blog.html'))
-  );
+module.exports = function(app) {
+  app.get("/", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
 
-  // cms route loads cms.html
-  app.get('/cms', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/cms.html'))
-  );
+  app.get("/login", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
 
-  // blog route loads blog.html
-  app.get('/blog', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/blog.html'))
-  );
-
-  // authors route loads author-manager.html
-  app.get('/authors', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/author-manager.html'))
-  );
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
 };
